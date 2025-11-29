00a4387f73fa974533f6bffbaf019aa2828b536a
Simon <>
2025-11-29 10:16:11 +0100

    opt

--- Guard_Jam_Katowice_2025/Assets/OptionButton.cs
+++ Guard_Jam_Katowice_2025/Assets/OptionButton.cs
@@ -1,10 +1,11 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class OptionButton : MonoBehaviour
 {
-    public Button Button { get; private set; }
-    public Text Text { get; private set; }
-    public Text descritption { get; private set; }
+    public Button Button;
+    public TextMeshProUGUI Text;
+    public TextMeshProUGUI descritption;
 
 }
