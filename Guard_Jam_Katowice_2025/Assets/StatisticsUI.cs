d5041e343233eda58e65738d9eda427f892299b8
Simon <>
2025-11-29 02:40:32 +0100

    add StatsUI

---
+++ Guard_Jam_Katowice_2025/Assets/StatisticsUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StatisticsUI : MonoBehaviour
+{
+    [Header("Sliders")]
+    [SerializeField] private Slider thirstSlider;
+    [SerializeField] private Slider hungerSlider;
+    [SerializeField] private Slider temperatureSlider;
+    [SerializeField] private Slider moraleSlider;
+
+    private void Start()
+    {
+        // Ustawienie sliderów na początkowe wartości
+        UpdateSliders();
+    }
+
+    private void Update()
+    {
+        // Aktualizacja sliderów w czasie rzeczywistym
+        UpdateSliders();
+    }
+
+    private void UpdateSliders()
+    {
+        if (StatisticsManager.Instance == null) return;
+
+        thirstSlider.value = StatisticsManager.Instance.thirst;
+        hungerSlider.value = StatisticsManager.Instance.hunger;
+        temperatureSlider.value = StatisticsManager.Instance.temperature;
+        moraleSlider.value = StatisticsManager.Instance.morale;
+    }
+}
