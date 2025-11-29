b4d656087a4af01de6f6cda1ec492724c5153fd4
Simon <>
2025-11-29 09:54:12 +0100

    some changes

--- Guard_Jam_Katowice_2025/Assets/DialogueController.cs
+++ Guard_Jam_Katowice_2025/Assets/DialogueController.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -26,18 +27,34 @@ public class DialogueController : MonoBehaviour
 
             text.text = choice.optionText;
 
-            // WIEEEEELKA UWAGA: zamykamy zmienną choice w lambda
             Choice captured = choice;
 
             btn.onClick.AddListener(() => OnChoiceSelected(captured));
         }
+
+
+    }
+    string GetDescription(Choice choice)
+    {
+        string desc = "";
+        if (choice.itemsYouUse.Length > 0)
+            desc += "Zużywa: " + string.Join(", ", Array.ConvertAll(choice.itemsYouUse, i => i.name)) + "\n";
+        if (choice.itemsYouGet.Length > 0)
+            desc += "Daje: " + string.Join(", ", Array.ConvertAll(choice.itemsYouGet, i => i.name)) + "\n";
+
+        desc += $"Hunger: {(choice.hungerChange >= 0 ? "+" : "")}{choice.hungerChange}, " +
+                $"Thirst: {(choice.waterChange >= 0 ? "+" : "")}{choice.waterChange}, " +
+                $"Morale: {(choice.moraleChange >= 0 ? "+" : "")}{choice.moraleChange}, " +
+                $"Temperature: {(choice.temperatureChange >= 0 ? "+" : "")}{choice.temperatureChange}";
+
+       return desc;
     }
     private void OnChoiceSelected(Choice choice)
     {
+        var stats = StatisticsManager.Instance;
+        var inv = InventoryManager.Instance;
 
         Debug.Log("Wybrano opcję: " + choice.optionText);
-     
-        StatisticsManager stats = StatisticsManager.Instance;
 
         stats.ChangeHunger(choice.hungerChange);
         stats.ChangeThirst(choice.waterChange);
@@ -46,6 +63,34 @@ public class DialogueController : MonoBehaviour
 
         Debug.Log($"H:{stats.hunger} W:{stats.thirst} T:{stats.temperature} M:{stats.morale}");
 
+        foreach (Item item in choice.itemsYouUse)
+        {
+            if (item != null && inv.HasItem(item.name))
+            {
+                inv.RemoveItemByName(item.name);
+                Debug.Log("Użyto item: " + item.name);
+            }
+            else if (item != null)
+            {
+                Debug.LogWarning("Brakuje itemu: " + item.name);
+            }
+        }
+
+        foreach (Item item in choice.itemsYouGet)
+        {
+            if (item != null)
+            {
+                if (!inv.IsInventoryFull())
+                {
+                    inv.AddItem(item);
+                    Debug.Log("Dodano item: " + item.name);
+                }
+                else
+                {
+                    Debug.LogWarning("Nie można dodać itemu, inventory pełne: " + item.name);
+                }
+            }
+        }
     }
 }
 
