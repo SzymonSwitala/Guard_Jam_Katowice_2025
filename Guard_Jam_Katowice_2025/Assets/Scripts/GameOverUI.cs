e0e7132998ac3ca6827a04871b8cca5c4a8e8ec7
Simon <>
2025-11-29 13:37:10 +0100

    ui

--- Guard_Jam_Katowice_2025/Assets/Scripts/GameOverUI.cs
+++ Guard_Jam_Katowice_2025/Assets/Scripts/GameOverUI.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class GameOverUI : MonoBehaviour
 {
     [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private GameObject UIPanel;
     private void Start()
     {
 
         gameOverPanel.SetActive(false);
+      
         StatisticsManager.Instance.OnAnyStatZero += ShowGameOver;
     }
 
     private void ShowGameOver()
     {
+        UIPanel.SetActive(false);
         gameOverPanel.SetActive(true);
     }
     private void OnDestroy()
